019c82ae6c07905499a1e534742a5640c614a24d
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-03-28 21:33:31 -0300

    s

---
+++ react-primeiro-projeto/components/Person.tsx
@@ -0,0 +1,11 @@
+export const Person =()=>{
+    return(
+        <>
+            <h1>Elon Musk</h1>
+            <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg/220px-Elon_Musk_Colorado_2022_%28cropped2%29.jpg" 
+            alt='Elon Musk'
+            />
+        </>
+    )
+}
--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -7,13 +7,14 @@ const Page = () => {
       <h1 className='font-bold text-2xl'>Olá mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <GeoForm />
+      
     </div>
   );
 }
 
 export default Page;
 // componentes -> geralmente inicia letra maiuscula
-// return () -> retorna apenas um elemento, para mais, usar div
+// return () -> components, retorna apenas um elemento, para mais, usar div dentro do return()
 // Export default -> usado ,de preferencia ,apenas para a propria Page
 // No 'import default', pode atribuir qulquer nome na const
+// <img /> -> precisa fechar sempre, com o />
