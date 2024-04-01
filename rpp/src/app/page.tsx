4c750e1fb1976bcba047fc7da6d4066aae8c7239
Willian Zytkoski <88804245+WillianZyt@users.noreply.github.com>
2024-04-01 19:38:19 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -4,26 +4,21 @@ import { peopleList } from "@/data/peopleList";
 
 const Page = () => {
 
-const chemists = peopleList.filter(person => person.profession === 'chemist')
+  const chemists = peopleList.filter(person => person.profession === 'chemist')
 
   return (
     <div>
       <h1 className='font-bold text-2xl'>Olá mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <ul>
-        {peopleList.map(person => 
-        <li>{person.name}</li>
-      )}
-      </ul>   
-      {chemists.length>0 && 
-      <>
-        <h3>Lista de químicos</h3>
-        <ul>
-          {chemists.map(person=>
-            <li key={person.id}>{person.name}</li>)}
-        </ul>
-      </>
+      {chemists.length > 0 &&
+        <>
+          <h3>Lista de químicos</h3>
+          <ul>
+            {chemists.map(person =>
+              <li key={person.id}>{person.name}</li>)}
+          </ul>
+        </>
       }
 
     </div>
