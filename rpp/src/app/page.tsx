9f918d5ae747eebc26fde35c2debb9d8fc1e4028
willian <willianzyt@outlook.com>
2024-04-03 19:00:53 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white 
     bg-gradient-to-r from-slate-500 to-indigo-500">
 
-      <EmojiRating rate={5}/>
+      <EmojiRating rate={2}/>
 
     </div>
   )
--- rpp/src/components/EmojiRating.tsx
+++ rpp/src/components/EmojiRating.tsx
@@ -7,14 +7,38 @@ export const EmojiRating = ({ rate }: Props) => {
     if (rate < 0) rate = 0;
 
     const rateInt = Math.floor(rate);
-
-    const stars = '😀'.repeat(rateInt) + '😶'.repeat(5 - rateInt)
+    let stars = '';
+    function starsEmoji(num){
+        num = rateInt;
+        if (rateInt >= 0 && rateInt < 1){
+            let stars = '😐'.repeat(5)
+            return stars;
+        }else if (rateInt >= 1 && rateInt < 2){
+            let stars = '😔'.repeat(5)
+            return stars;
+        }else if (rateInt >= 2 && rateInt < 3){
+            let stars = '🙂'.repeat(5)
+            return stars;
+        }else if (rateInt >= 3 && rateInt < 4){
+            let stars = '🙃'.repeat(5)
+            return stars;
+        }else if (rateInt >= 4 && rateInt < 5){
+            let stars = '😇'.repeat(5)
+            return stars;
+        }else if(rateInt >= 5){
+            let stars = '😀'.repeat(5)
+            return stars;
+        }
+    }
+    
+    
+    // const stars = '😀'.repeat(rateInt) + '😶'.repeat(5 - rateInt)
 
 
     return (
         <div className="flex items-center text-6xl">
             <div className="bg-gray-700 p-2 rounded">{rate.toFixed(1)}</div>
-            <div className="ml-3">{stars}</div>
+            <div className="ml-3">{starsEmoji(stars)}</div>
         </div>
     )
 }
