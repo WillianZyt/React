9201e2a84a431d57ad7fa176c22c375f97638e6a
willian <willianzyt@outlook.com>
2024-04-03 23:39:15 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -1,18 +1,12 @@
+import { StudentTable } from "@/components/StudentTable";
 import { students } from "@/data/students";
-import { Estudantes } from "@/components/Estudantes";
+
 
 const Page = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white 
-    bg-gradient-to-r from-slate-500 to-indigo-500">
-      <ul>
-      <Estudantes id={0}/>
-      <Estudantes id={1}/>
-      <Estudantes id={2}/>
-      <Estudantes id={3}/>
-      <Estudantes id={4}/>
-      <Estudantes id={5}/>
-      </ul>
+    <div className="container mx-auto">
+      <h1 className="text-2xl">Lista de estudantes</h1>
+      <StudentTable students={students}/>
     </div>
   );
 }
