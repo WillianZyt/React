735968346023c04645bbd942c6baa43fac6f9f28
willian <willianzyt@outlook.com>
2024-04-07 23:13:50 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -1,11 +1,25 @@
 'use client'
 
+import { PhotoItem } from "@/components/PhotoItem";
+import { photoList } from "@/data/photoList";
 import { useState } from "react";
 
 const Page = () => {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="w-screen h-screen flex flex-col items-center text-2xl">
-      <h1 className="text-4xl mt-5">Teste update</h1>
+    <div className="mx-2">
+      <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalacticas</h1>
+
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {photoList.map(item => (
+          <PhotoItem
+          key={item.id}
+          photo={item}
+          onClick={()=>{}}
+          />
+        ))}
+      </section>
     </div>
   );
 }
