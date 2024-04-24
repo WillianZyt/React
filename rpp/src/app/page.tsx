ff5365f0fc05c4584b26b43a406a658d535fb152
willian <willianzyt@outlook.com>
2024-04-23 23:53:42 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -1,16 +1,26 @@
 'use client'
 
-import { Header } from "@/components/Header";
-import { DarkModeProvider, ThemeProvider } from "@/contexts/DarkModeContext";
+import { Button } from "@/components/Button";
+import { Container } from "@/components/Container";
+import { TheSwitch } from "@/components/ThemeSwitch";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const Page = () => {
 
   return (
-    <DarkModeProvider>
-      <div className="container mx-auto">
-        <Header />
-      </div>
-    </DarkModeProvider>
+    <ThemeProvider>
+      <Container>
+        <header className="py-5">
+          <h1 className="text-3xl">Título da página</h1>
+        </header>
+        <section>
+          <p className="my-5">Conteúdo da Página</p>
+          <Button label="Clique aqui" onClick={() => { }} />
+        </section>
+        <TheSwitch />
+      </Container>
+    </ThemeProvider>
+
 
   );
 }
