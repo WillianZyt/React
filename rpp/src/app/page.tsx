68dff7890ac4b842b25f89e90667c1ee38ce42b7
willian <willianzyt@outlook.com>
2024-04-25 23:06:49 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -1,26 +1,16 @@
 'use client'
 
-import { Button } from "@/components/Button";
-import { Container } from "@/components/Container";
-import { TheSwitch } from "@/components/ThemeSwitch";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { Chat } from "@/components/Chat";
+import { PostProvider } from "@/contexts/PostContext";
 
 const Page = () => {
 
   return (
-    <ThemeProvider>
-      <Container>
-        <header className="py-5">
-          <h1 className="text-3xl">Título da página</h1>
-        </header>
-        <section>
-          <p className="my-5">Conteúdo da Página</p>
-          <Button label="Clique aqui" onClick={() => { }} />
-        </section>
-        <TheSwitch />
-      </Container>
-    </ThemeProvider>
-
+    <PostProvider>
+      <div>
+        <Chat />
+      </div>
+    </PostProvider>
 
   );
 }
