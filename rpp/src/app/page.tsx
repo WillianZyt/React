cd77e37e0afba63ceec8aa52b48cbb98d8a468ed
Willian Zytkoski <willianzyt@outlook.com>
2024-04-28 15:53:31 -0300

    s

--- rpp/src/app/page.tsx
+++ rpp/src/app/page.tsx
@@ -1,22 +1,78 @@
 'use client'
 
-import { Chat } from "./components/Chat";
-import { ChatProvider } from "./contexts/chatContext";
-import { UserProvider } from "./contexts/userContext";
+import { User } from "@/types/User";
+import { title } from "process";
+import { useEffect, useState } from "react";
 
 const Page = () => {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const json = await res.json();
+      setUsers(json);
+
+    } catch (err) {
+      console.log('DEU ALGO ERRADO');
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const handleAddNewPost = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'aplication/json; charset=UTF-8'
+      },
+      body: JSON.stringify({
+        title: 'Post de teste',
+        body: 'Corpo de teste',
+        userId: 99
+      })
+    });
+    const json = await res.json();
+    console.log(json)
+  }
 
   return (
     <div className="container mx-auto max-w-lg px-2">
-      <UserProvider>
-        <ChatProvider>
-          <h1 className="text-3xl my-3 text-center">Chat simples</h1>
-
-          <Chat/>
-        </ChatProvider>
-      </UserProvidegr>
+      <button
+        className="bg-white/30 my-1 p-1 rounded-md"
+        onClick={handleAddNewPost}
+      >Adicionar Novo Post</button>
+      <h1 className="text-3xl">Lista de usuários</h1>
+      {loading && 'Carregando...'}
+      {users.length > 0 &&
+        <ul>
+          {users.map(item => (
+            <li>{item.name}</li>
+          ))}
+        </ul>
+      }
+      {users.length === 0 && 'Não há usuários para exibir'}
     </div>
   );
 }
 
 export default Page;
+
+// useEffect(() => {
+//   setLoading(true);
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(json => {
+//       setLoading(false);
+//       setUsers(json);
+//     })
+//     .catch(()=>{
+//       setLoading(false);
+//       console.log('DEU ALGUM ERRO');
+//     })
+// }, []);
--- rpp/src/types/Message.ts
+++
@@ -1,5 +0,0 @@
-export type Message ={
-  id: number;
-  user: string;
-  text: string;
-}
